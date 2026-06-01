ed0f859205244dd877c53cae4c74f8a111f0f938
Miliritgithub <tm045958@gmail.com>
2026-06-02 01:32:38 +0530

    somthing change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
